33340bc0d978c4595ca11f9f9f8987dc1df8c78f
Abigail Licata <46607449+licataae@users.noreply.github.com>
2023-10-15 17:10:43 +0200

    Update batch-desaturate.scm
    updated definition for "mode" to "desaturate-mode"

--- scripts/batch-desaturate.scm
+++ scripts/batch-desaturate.scm
@@ -1,14 +1,12 @@
   (define (batch-drawable-desaturate pattern
-                              mode
-                              )
+				     desaturate-mode)
   (let* ((filelist (cadr (file-glob pattern 1))))
     (while (not (null? filelist))
            (let* ((filename (car filelist))
                   (image (car (gimp-file-load RUN-NONINTERACTIVE
                                               filename filename)))
                   (drawable (car (gimp-image-get-active-layer image))))
-             (gimp-drawable-desaturate RUN-NONINTERACTIVE
-                                   image drawable radius amount threshold)
+             (gimp-drawable-desaturate drawable desaturate-mode)
              (gimp-file-save RUN-NONINTERACTIVE
                              image drawable filename filename)
              (gimp-image-delete image))
